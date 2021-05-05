788ee46e9b0a9e0dc82f617a2094b0e86d80f75d
katerina-spb <katespb98@gmail.com>
2021-05-05 15:04:10 -0500

    chunk solved

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //create result array
+  var result = [];
+  //create currentSubarray
+  var currentSub = [];
+  result.push(currentSub);
+  //create counter
+  var counter = 0;
+  //iterate over an array
+  for (element of array) {
+    //if counter less than size
+    if (counter < size) {
+      //push the element to current subarray
+      currentSub.push(element);
+      counter ++;
+    } else {
+      // else - create new subarray and reassign current subarray to it
+      var newSubArray = [];
+      result.push(newSubArray);
+      currentSub = newSubArray;
+      // set counter to 0
+      counter = 1;
+      //push the element to current subarray
+      currentSub.push(element);
+    }
+  }
+  //return result
+  return result;
+}
 
 module.exports = chunk;
