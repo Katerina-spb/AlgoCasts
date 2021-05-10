5c855d2e77f3f5afc0929c2ab9d7f77d981cf3a5
katerina-spb <katespb98@gmail.com>
2021-05-10 15:33:44 -0500

    capitalize solved

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,13 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  var words = str.split(' ');
+  for (var i = 0; i < words.length; i ++) {
+    first = words[i][0].toUpperCase();
+    words[i] = first + words[i].slice(1);
+  }
+  return words.join(' ');
+}
 
 module.exports = capitalize;
